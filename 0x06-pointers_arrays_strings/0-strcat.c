69eed66e1e3fc92d7ffd268b8a478f229b6766bd
Toonswill <ngunjiriwilson10@gmail.com>
2023-03-02 21:09:01 +0300

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,17 +14,17 @@ char *_strcat(char *dest, char *src)
 	i = 0;
 	j = 0;
 
-	while (dest[i] != `\0`)
+	while (dest[i] != '\0')
 		i++;
 
-       	while (src[j] != `\0`)
+	while (src[j] != '\0')
 	{
-		dest[i] = src[j]
+		dest[i] = src[j];
 		j++;
 		i++;
 	}
 
-	dest[i] = `\0`;
-	
+	dest[i] = '\0';
+
 	return (dest);
 }
