c5ef3068ebdabde444068c369f6d9c207cee9454
toonswill <ngunjiriwilson10@gmail.com>
2023-06-09 23:04:17 +0300

    updats

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,11 +1,19 @@
-nclude "main.h"
+#include "main.h"
+
 /**
- * _islower - checks for lowercase character
- * @c: the character to check
- * Return: 1 if c is lowercase, 0 otherwise
- */
+ * _islower - checks for lowercase character.
+ * @c: integer parameter to check
+ * Return: 1 if c is lowercase 0 otherwise
+*/
 int _islower(int c)
 {
-	return (c >= 'a' && c <= 'z');
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
 }
-
